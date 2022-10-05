d31d6a1f7ae8f8682b34d9abe30f74fc65470488
kirill-churin <kirillchurin07@mail.ru>
2022-10-05 21:28:11 +0300

    Update sensor

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,7 +1,8 @@
 #include <GyverBME280.h>
 #include <LiquidCrystal.h>
 
-#define button_toggle_pin 6
+#define button_toggle_display_pin 6
+#define button_toggle_cond_pin 5
 #define temp_cond 0
 #define pres_cond 1
 
@@ -9,46 +10,70 @@ LiquidCrystal lcd(9, 8, 7, 10, 11, 12);
 GyverBME280 bme;
 int stat = temp_cond;
 bool wasUp = true;
-const float pressure_delta = 101200, temperature_delta = 27.5; //временно
+float pressure_delta = 101200, temperature_delta = 27.5; //нормальные показтель температуры и давления по умолчанию: 101200 Па и 27.5 градусов Цельсия
+float sum_pressure = 0, sum_temperature = 0;
 
 void setup()
 {
   lcd.begin(16, 2);
   bme.begin(0x76);
-  pinMode(button_toggle_pin, INPUT_PULLUP);
+  pinMode(button_toggle_display_pin, INPUT_PULLUP);
+  pinMode(button_toggle_cond_pin, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 void loop()
 {
-  bool isUp = digitalRead(button_toggle_pin);
-  float pressure = bme.readPressure();
-  float temperature = bme.readTemperature();
-  if (pressure < pressure_delta || temperature > temperature_delta)
-    digitalWrite(LED_BUILTIN, HIGH);
-  else
-    digitalWrite(LED_BUILTIN, LOW);
-  if (wasUp && !isUp)
+  if (!digitalRead(button_toggle_cond_pin))
   {
-    delay(50);
-    isUp = digitalRead(button_toggle_pin);
-    if (!isUp)
+    lcd.clear();
+    lcd.print("...");
+    for (int i = 0; i < 5; i++)
     {
-      stat = 1 - stat;
+      sum_pressure += bme.readPressure();
+      sum_temperature += bme.readTemperature();
+      delay(200);
     }
+    // 5 - кол-во вычислений. Пока 5
+    pressure_delta = sum_pressure / 5;
+    temperature_delta = sum_temperature / 5;
+    sum_pressure = 0; sum_temperature = 0;
+    lcd.clear();
   }
-  wasUp = isUp;
-  
-  if (stat == temp_cond)
+  else
   {
-    lcd.setCursor(0, 0);
-    lcd.print(pressure);
-    lcd.print(" Pa      ");
+    bool isUp = digitalRead(button_toggle_display_pin);
+    float pressure = bme.readPressure();
+    float temperature = bme.readTemperature();
+    // Проверка на несоответствие нормальным показателям
+    if (pressure < pressure_delta - 1000 || temperature > temperature_delta + 0.2) // Учитываем погрешность вычислений
+    {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(100);
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+    else
+      digitalWrite(LED_BUILTIN, LOW);
+    if (wasUp && !isUp)
+    {
+      delay(50);
+      isUp = digitalRead(button_toggle_display_pin);
+      if (!isUp)
+        stat = 1 - stat;
+    }
+    wasUp = isUp;
+    
+    if (stat == temp_cond)
+    {
+      lcd.setCursor(0, 0);
+      lcd.print(pressure);
+      lcd.print(" Pa      ");
+    }
+    else if (stat == pres_cond)
+    {
+      lcd.setCursor(0, 0);
+      lcd.print(temperature);
+      lcd.print(" " + String(char(223)) + "C      ");
+    }
+    delay(100);
   }
-  else if (stat == pres_cond)
-  {
-    lcd.setCursor(0, 0);
-    lcd.print(temperature);
-    lcd.print(" " + String(char(223)) + "C      ");
-  } 
-  delay(100);
 }
--- Test/Test/Test.ino
+++ Test/Test/Test.ino
@@ -10,42 +10,49 @@ LiquidCrystal lcd(9, 8, 7, 10, 11, 12);
 GyverBME280 bme;
 int stat = temp_cond;
 bool wasUp = true;
-int cond = 1; // 1 - вычисление нормальных показателей, 2 - основная работа
-float pressure_delta = 101200, temperature_delta = 27.5; //нормальные показтель температуры и давления по умолчанию: 101200 Па и 27.5 градусовC
-
-float calculateAverage(float arr[], int len);
+float pressure_delta = 101200, temperature_delta = 27.5; //нормальные показтель температуры и давления по умолчанию: 101200 Па и 27.5 градусов Цельсия
+float sum_pressure = 0, sum_temperature = 0;
 
 void setup()
 {
   lcd.begin(16, 2);
   bme.begin(0x76);
+  // Serial.begin(9600); Временно
   pinMode(button_toggle_display_pin, INPUT_PULLUP);
   pinMode(button_toggle_cond_pin, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 void loop()
 {
-  if (digitalRead(button_toggle_display_pin))
-    cond = 1 - cond;
-  if (cond == 1)
+  if (!digitalRead(button_toggle_cond_pin))
   {
-    float pressure_data[5], temperature_data[5];
+    lcd.clear();
+    lcd.print("...");
     for (int i = 0; i < 5; i++)
     {
-      pressure_data[i] = bme.readPressure();
-      temperature_data[i] = bme.readTemperature();
+      sum_pressure += bme.readPressure();
+      sum_temperature += bme.readTemperature();
       delay(200);
     }
-    pressure_delta = calculateAverage(pressure_data, 5);
-    temperature_delta = calculateAverage(temperature_data, 5);
+    // 5 - кол-во вычислений. Пока 5
+    pressure_delta = sum_pressure / 5;
+    temperature_delta = sum_temperature / 5;
+    sum_pressure = 0; sum_temperature = 0;
+    lcd.clear();
+    // Serial.println(String(pressure_delta) + " " + String(temperature_delta)); Временно
   }
   else
   {
     bool isUp = digitalRead(button_toggle_display_pin);
     float pressure = bme.readPressure();
     float temperature = bme.readTemperature();
-    if (pressure < pressure_delta || temperature > temperature_delta)
+    // Проверка на несоответствие нормальным показателям
+    if (pressure < pressure_delta - 1000 || temperature > temperature_delta + 0.2) // Учитываем погрешность вычислений
+    {
       digitalWrite(LED_BUILTIN, HIGH);
+      delay(100);
+      digitalWrite(LED_BUILTIN, LOW);
+    }
     else
       digitalWrite(LED_BUILTIN, LOW);
     if (wasUp && !isUp)
@@ -71,12 +78,4 @@ void loop()
     }
     delay(100);
   }
-}
-
-float calculateAverage(float arr[], float len)
-{
-  float sum = 0;
-  for (int i = 0; i < len; i++)
-    sum += arr[i];
-  return sum / len;
 }
